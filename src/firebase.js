c245272df70ae46f672809dc5b78696339c6011f
dhavalvaja <74464000+dhavalvaja@users.noreply.github.com>
2022-07-30 21:39:11 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth"
 import { getFirestore } from 'firebase/firestore'
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyClGDTbBuZLs0PeAsQbXjDmULvrL628T_E",
-  authDomain: "insurance-project-1c37c.firebaseapp.com",
-  projectId: "insurance-project-1c37c",
-  storageBucket: "insurance-project-1c37c.appspot.com",
-  messagingSenderId: "835226519991",
-  appId: "1:835226519991:web:62b89017b1a35900c08032"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 });
 
 export const auth = app.auth(); 
